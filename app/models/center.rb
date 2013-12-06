e9f02f02043a6414e3e8390c207418152692af8e
Evanson Wachira <evansonwachira@gmail.com>
2013-12-06 08:57:26 +0300

    Enabled cascaded destroy of center and student days

--- app/models/center.rb
+++ app/models/center.rb
@@ -1,8 +1,24 @@
 class Center < ActiveRecord::Base
+  CENTER_DAYS_MIN = 1
   validates :name, presence: true, uniqueness: { case_sensitive: false}
   has_many :instructors, :dependent => :nullify
   has_many :students, :dependent => :nullify
   has_many :center_days, :dependent => :destroy
   has_many :school_days, :through => :center_days
   accepts_nested_attributes_for :school_days
+  validate do
+    check_center_days_count
+  end
+
+  private
+
+  def center_day_count_valid?
+    center_days.count >= CENTER_DAYS_MIN
+  end
+
+  def check_center_days_count
+    unless center_day_count_valid?
+      errors.add(:base, "Center must have at least 1 day active")
+    end
+  end
 end
--- app/models/center_day.rb
+++ app/models/center_day.rb
@@ -1,6 +1,9 @@
 class CenterDay < ActiveRecord::Base
   belongs_to :center
   belongs_to :school_day
-  has_many :student_days
+  has_many :student_days, :dependent => :destroy
   has_many :students, :through => :student_days
+
+  before_destroy { |record| StudentDay.destroy_all "center_day_id = #{record.id}"}
+
 end
